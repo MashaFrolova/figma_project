a0708975baa4b3221806c0eef4216757f3dd9afc
Masha <mashka-f@yandex.ru>
2021-11-25 23:28:11 +0300

    1200

--- page-product.php
+++ page-product.php
@@ -13,9 +13,7 @@ Template Name: Product
                 <div class="macbook__wrapper_content">
                     <h2 class="header" ><?php the_field('macbook_title'); ?></h2>
                     <h4 class="subheader" ><?php the_field('macbook_descr'); ?></h4>
-                    <div>
-                        <button class="button button_macbook">Try For Free</button>
-                    </div>
+                    <button class="button button_macbook">Try For Free</button>
                 </div>                
                 <div class="macbook__wrapper_screen">
                     <img src="<?php the_field(macbook_img); ?>">
